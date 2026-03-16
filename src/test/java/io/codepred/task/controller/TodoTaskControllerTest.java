9a22c8ccbc25395eda4d52b662e83f343c07c290
Maksym Uimanov <uimanovmaks@gmail.com>
2026-03-16 01:12:02 +0100

    Add controller and service layer tests for Todo Task API:
    - Added `TodoTaskServiceTest` to validate all service methods, including edge cases.
    - Introduced `TodoTaskControllerTest` to test API endpoints with MockMvc.
    - Covered scenarios for create, read, update, and delete operations.

---
+++ src/test/java/io/codepred/task/controller/TodoTaskControllerTest.java
@@ -0,0 +1,141 @@
+package io.codepred.task.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import io.codepred.task.dto.TodoTaskRequest;
+import io.codepred.task.dto.TodoTaskResponse;
+import io.codepred.task.entity.TodoStatus;
+import io.codepred.task.service.TodoTaskService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jackson.autoconfigure.JacksonAutoConfiguration;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@WebMvcTest(TodoTaskController.class)
+@Import(JacksonAutoConfiguration.class)
+class TodoTaskControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockitoBean
+    private TodoTaskService todoTaskService;
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        objectMapper = JsonMapper.builder()
+                .addModule(new JavaTimeModule())
+                .build();
+    }
+
+    @Test
+    void postTodoTask_shouldCreateTodoTask() throws Exception {
+        TodoTaskRequest todoTaskRequest = new TodoTaskRequest("Title", null, TodoStatus.NOT_STARTED);
+        TodoTaskResponse todoTaskResponse = new TodoTaskResponse(1L, todoTaskRequest.title(), todoTaskRequest.description(), todoTaskRequest.status(), LocalDateTime.now());
+
+        when(todoTaskService.createTodoTask(todoTaskRequest))
+                .thenReturn(todoTaskResponse);
+        mockMvc.perform(post("/api/v1.0/tasks")
+                        .content(objectMapper.writeValueAsString(todoTaskRequest))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(todoTaskResponse.id()))
+                .andExpect(jsonPath("$.title").value(todoTaskResponse.title()))
+                .andExpect(jsonPath("$.description").value(todoTaskResponse.description()))
+                .andExpect(jsonPath("$.status").value(todoTaskResponse.status().name()))
+                .andExpect(jsonPath("$.createdAt").exists());
+        verify(todoTaskService)
+                .createTodoTask(todoTaskRequest);
+    }
+
+    @Test
+    void getTodoTasks_shouldReturnTodoTaskPage() throws Exception {
+        TodoTaskResponse todoTaskResponse = new TodoTaskResponse(1L, "Title", null, TodoStatus.NOT_STARTED, LocalDateTime.now());
+        PageImpl<TodoTaskResponse> todoTaskPage = new PageImpl<>(List.of(todoTaskResponse));
+
+        when(todoTaskService.getTodoTasks(any()))
+                .thenReturn(todoTaskPage);
+
+        mockMvc.perform(get("/api/v1.0/tasks")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content[0].id").value(todoTaskResponse.id()))
+                .andExpect(jsonPath("$.content[0].title").value(todoTaskResponse.title()))
+                .andExpect(jsonPath("$.content[0].description").value(todoTaskResponse.description()))
+                .andExpect(jsonPath("$.content[0].status").value(todoTaskResponse.status().name()))
+                .andExpect(jsonPath("$.content[0].createdAt").exists());
+        verify(todoTaskService)
+                .getTodoTasks(any());
+    }
+
+    @Test
+    void getTodoTask_shouldReturnTodoTask() throws Exception {
+        Long id = 1L;
+        TodoTaskResponse todoTaskResponse = new TodoTaskResponse(id, "Title", null, TodoStatus.NOT_STARTED, LocalDateTime.now());
+
+        when(todoTaskService.getTodoTask(id))
+                .thenReturn(todoTaskResponse);
+
+        mockMvc.perform(get("/api/v1.0/tasks/{id}", id)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(todoTaskResponse.id()))
+                .andExpect(jsonPath("$.title").value(todoTaskResponse.title()))
+                .andExpect(jsonPath("$.description").value(todoTaskResponse.description()))
+                .andExpect(jsonPath("$.status").value(todoTaskResponse.status().name()))
+                .andExpect(jsonPath("$.createdAt").exists());
+        verify(todoTaskService)
+                .getTodoTask(id);
+    }
+
+    @Test
+    void putTodoTask_shouldUpdateTodoTask() throws Exception {
+        Long id = 1L;
+        TodoTaskRequest todoTaskRequest = new TodoTaskRequest("Title", null, TodoStatus.NOT_STARTED);
+        TodoTaskResponse todoTaskResponse = new TodoTaskResponse(id, todoTaskRequest.title(), todoTaskRequest.description(), todoTaskRequest.status(), LocalDateTime.now());
+
+        when(todoTaskService.updateTodoTask(id, todoTaskRequest))
+                .thenReturn(todoTaskResponse);
+
+        mockMvc.perform(put("/api/v1.0/tasks/{id}", id)
+                .content(objectMapper.writeValueAsString(todoTaskRequest))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(todoTaskResponse.id()))
+                .andExpect(jsonPath("$.title").value(todoTaskResponse.title()))
+                .andExpect(jsonPath("$.description").value(todoTaskResponse.description()))
+                .andExpect(jsonPath("$.status").value(todoTaskResponse.status().name()))
+                .andExpect(jsonPath("$.createdAt").exists());
+        verify(todoTaskService)
+                .updateTodoTask(id, todoTaskRequest);
+    }
+
+    @Test
+    void deleteTodoTask_shouldDeleteTodoTask() throws Exception {
+        Long id = 1L;
+
+        mockMvc.perform(delete("/api/v1.0/tasks/{id}", id)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        verify(todoTaskService)
+                .deleteTodoTask(id);
+    }
+}
--- src/test/java/io/codepred/task/service/TodoTaskServiceTest.java
+++ src/test/java/io/codepred/task/service/TodoTaskServiceTest.java
@@ -4,6 +4,7 @@ import io.codepred.task.dto.TodoTaskRequest;
 import io.codepred.task.dto.TodoTaskResponse;
 import io.codepred.task.entity.TodoStatus;
 import io.codepred.task.entity.TodoTask;
+import io.codepred.task.exception.TodoTaskNotFoundException;
 import io.codepred.task.mapper.TodoTaskMapper;
 import io.codepred.task.repository.TodoTaskRepository;
 import org.junit.jupiter.api.Test;
@@ -11,12 +12,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class TodoTaskServiceTest {
@@ -54,5 +60,111 @@ class TodoTaskServiceTest {
                 .save(todoTask);
         verify(todoTaskMapper)
                 .toTodoTaskResponse(todoTask);
+    }
+
+    @Test
+    void getTodoTasks_shouldReturnTodoTaskPage() {
+        Long id = 1L;
+        TodoTask todoTask = new TodoTask(id, "Title", null, TodoStatus.NOT_STARTED, LocalDateTime.now());
+        TodoTaskResponse todoTaskResponse = new TodoTaskResponse(todoTask.getId(), todoTask.getTitle(), todoTask.getDescription(), todoTask.getStatus(), todoTask.getCreatedAt());
+        Page<TodoTask> todoTaskPage = new PageImpl<>(List.of(todoTask));
+        Pageable pageable = mock(Pageable.class);
+
+        when(todoTaskRepository.findAll(pageable))
+                .thenReturn(todoTaskPage);
+        when(todoTaskMapper.toTodoTaskResponse(todoTask))
+                .thenReturn(todoTaskResponse);
+
+        Page<TodoTaskResponse> todoTasks = todoTaskService.getTodoTasks(pageable);
+
+        assertThat(todoTasks)
+                .isNotNull()
+                .hasSize(1)
+                .contains(todoTaskResponse);
+        verify(todoTaskRepository)
+                .findAll(pageable);
+        verify(todoTaskMapper)
+                .toTodoTaskResponse(todoTask);
+    }
+
+    @Test
+    void getTodoTask_shouldReturnTodoTask_whenTodoTaskExists() {
+        Long id = 1L;
+        TodoTask todoTask = new TodoTask(id, "Title", null, TodoStatus.NOT_STARTED, LocalDateTime.now());
+        TodoTaskResponse todoTaskResponse = new TodoTaskResponse(todoTask.getId(), todoTask.getTitle(), todoTask.getDescription(), todoTask.getStatus(), todoTask.getCreatedAt());
+
+        when(todoTaskRepository.findById(id))
+                .thenReturn(Optional.of(todoTask));
+        when(todoTaskMapper.toTodoTaskResponse(todoTask))
+                .thenReturn(todoTaskResponse);
+
+        TodoTaskResponse retrievedTodoTask = todoTaskService.getTodoTask(id);
+
+        assertThat(retrievedTodoTask)
+                .isNotNull()
+                .isEqualTo(todoTaskResponse);
+        verify(todoTaskRepository)
+                .findById(id);
+        verify(todoTaskMapper)
+                .toTodoTaskResponse(todoTask);
+    }
+
+    @Test
+    void getTodoTask_shouldThrowException_whenTodoTaskNotExists() {
+        Long id = 1L;
+
+        when(todoTaskRepository.findById(id))
+                .thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> todoTaskService.getTodoTask(id))
+                .isInstanceOf(TodoTaskNotFoundException.class);
+
+        verify(todoTaskRepository)
+                .findById(id);
+        verify(todoTaskMapper, never())
+                .toTodoTaskResponse(any());
+    }
+
+    @Test
+    void updateTodoTask_shouldConvertRequestAndUpdateTodoTask() {
+        Long id = 1L;
+        TodoTaskRequest todoTaskRequest = new TodoTaskRequest("Title", null, TodoStatus.NOT_STARTED);
+        TodoTask todoTask = spy(new TodoTask(null, todoTaskRequest.title(), todoTaskRequest.description(), todoTaskRequest.status(), LocalDateTime.now()));
+        TodoTaskResponse todoTaskResponse = new TodoTaskResponse(todoTask.getId(), todoTask.getTitle(), todoTask.getDescription(), todoTask.getStatus(), todoTask.getCreatedAt());
+
+        when(todoTaskMapper.toTodoTask(todoTaskRequest))
+                .thenReturn(todoTask);
+        when(todoTaskRepository.save(todoTask))
+                .thenReturn(todoTask);
+        when(todoTaskMapper.toTodoTaskResponse(todoTask))
+                .thenReturn(todoTaskResponse);
+
+        TodoTaskResponse result = todoTaskService.updateTodoTask(id, todoTaskRequest);
+
+        assertThat(result)
+                .isNotNull()
+                .isEqualTo(todoTaskResponse);
+        verify(todoTaskMapper)
+                .toTodoTask(todoTaskRequest);
+        verify(todoTask)
+                .setId(id);
+        verify(todoTaskRepository)
+                .save(todoTask);
+        verify(todoTaskMapper)
+                .toTodoTaskResponse(todoTask);
+    }
+
+    @Test
+    void deleteTodoTask_shouldDeleteTodoTask() {
+        Long id = 1L;
+
+        doNothing()
+                .when(todoTaskRepository)
+                .deleteById(id);
+
+        todoTaskService.deleteTodoTask(id);
+
+        verify(todoTaskRepository)
+                .deleteById(id);
     }
 }
